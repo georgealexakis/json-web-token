24122ff1e5f852f9bf594c74066652ef140066c1
georgealexakis <geosalexs@gmail.com>
2021-08-14 21:29:55 +0300

    Init

---
+++ server.js
@@ -0,0 +1,67 @@
+const _ = require('lodash');
+const express = require('express')
+const app = express();
+const jwt = require('jsonwebtoken');
+const expressJwt = require('express-jwt');
+const cors = require('cors');
+const corsOptions = { origin: '*' };
+// Demo data
+const LIST = [
+    { 'listid': 1, 'userid': 1, 'name': 'Car', 'completed': false },
+    { 'listid': 2, 'userid': 1, 'name': 'PC', 'completed': true },
+    { 'listid': 3, 'userid': 2, 'name': 'Robot', 'completed': false },
+    { 'listid': 4, 'userid': 3, 'name': 'Angular', 'completed': false }
+];
+const USERS = [
+    { 'userid': 1, 'username': 'george', 'password': '1111' },
+    { 'userid': 2, 'username': 'peter', 'password': '1234' },
+    { 'userid': 3, 'username': 'john', 'password': '4321' },
+];
+function getList(userID) {
+    const list = _.filter(LIST, ['userid', userID]);
+    return list;
+}
+function getItem(itemID) {
+    const item = _.find(LIST, (item) => { return item.id === itemID; })
+    return item;
+}
+function getUsers() {
+    return USERS;
+}
+// API options
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(expressJwt({ secret: 'soup-app-secret', algorithms: ['HS256'] }).unless({ path: ['/api/auth'] }));
+app.use((err, _req, res, _next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).send({ message: 'Invalid token' });
+    }
+});
+// Routes
+app.get('/', (_req, res) => {
+    res.json({ message: 'JWT API Server' });
+});
+app.post('/api/auth', (req, res) => {
+    const body = req.body;
+    const user = USERS.find(user => user.username === body.username);
+    if (!user || body.password !== user.password) return res.sendStatus(401);
+    const token = jwt.sign({ userID: user.userid }, 'soup-app-secret', { expiresIn: '2h' });
+    res.send({ token });
+});
+app.get('/api/list', (req, res) => {
+    res.type('json');
+    res.send(getList(req.user.userID));
+});
+app.get('/api/list/:id', (req, res) => {
+    const itemID = req.params.id;
+    res.type('json');
+    res.send(getItem(itemID));
+});
+app.get('/api/users', (_req, res) => {
+    res.type('json');
+    res.send(getUsers());
+});
+app.listen(4000, () => {
+    console.log('JWT API Server listening on port 4000');
+});
